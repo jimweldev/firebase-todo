24ba5891173b6db81ad5be1435e6113f8e1ca383
Jimwel Dizon <jimweldizon.dev@gmail.com>
2022-07-19 11:44:33 +0800

    Added Todo CRUD

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,30 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { signOut } from 'firebase/auth'
-import { collection, getDocs } from 'firebase/firestore'
+import {
+  collection,
+  onSnapshot,
+  getDocs,
+  addDoc,
+  query,
+  where,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from 'firebase/firestore'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../../firebaseConfig'
 
 const User = () => {
+  // user
   const [user] = useAuthState(auth)
-  const [todos, setTodos] = useState([])
+  const uid = user.uid
 
+  // todos
+  const [todos, setTodos] = useState([])
   const todosCollectionRef = collection(db, 'todos')
 
+  // update database realtime
   useEffect(() => {
-    getTodos()
+    const q = query(todosCollectionRef, where('userId', '==', uid))
+
+    onSnapshot(q, (snapshot) =>
+      setTodos(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    )
   }, [])
 
-  const getTodos = async () => {
-    const data = await getDocs(todosCollectionRef)
+  // useEffect(() => {
+  //   getTodos()
+  // }, [])
 
-    setTodos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-  }
+  // const getTodos = async () => {
+  //   const q = query(todosCollectionRef, where('userId', '==', uid))
+
+  //   const data = await getDocs(q)
+  //   setTodos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+  // }
 
   return (
     <>
       <div>{user.displayName}</div>
-      <div>{user.uid}</div>
       <img src={user.photoURL} alt="" />
+
+      <br />
+
       <button
         onClick={() => {
           signOut(auth)
@@ -32,9 +57,56 @@ const User = () => {
       >
         Sign out
       </button>
-      {todos.map((todo) => {
-        return <div key={todo.id}>{todo.title}</div>
-      })}
+
+      <br />
+      <br />
+
+      <button
+        onClick={() => {
+          addDoc(todosCollectionRef, {
+            userId: uid,
+            title: 'New Title',
+            status: 'pending',
+          })
+        }}
+      >
+        Add Todo
+      </button>
+
+      {todos.length === 0 ? (
+        <div>No Todos</div>
+      ) : (
+        todos.map((todo) => {
+          return (
+            <div key={todo.id}>
+              <div>
+                <div>{todo.title}</div>
+                <div>{todo.status}</div>
+                <button
+                  onClick={() => {
+                    const todoDoc = doc(db, 'todos', todo.id)
+                    const newFields = { status: 'finished' }
+
+                    updateDoc(todoDoc, newFields)
+                  }}
+                >
+                  done
+                </button>
+                <button
+                  onClick={() => {
+                    const todoDoc = doc(db, 'todos', todo.id)
+
+                    deleteDoc(todoDoc)
+                  }}
+                >
+                  delete
+                </button>
+              </div>
+              <br />
+            </div>
+          )
+        })
+      )}
     </>
   )
 }
